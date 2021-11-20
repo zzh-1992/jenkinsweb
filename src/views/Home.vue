b96ef7fb51c4f998db6f2802a90ca15e71cae03b
zzh-1992 <ezzh1992@icloud.com>
2021-11-20 21:28:13 +0800

    添加markdown功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,30 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Markdown></Markdown>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Markdown from "./Markdown";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    Markdown,
   },
 };
 </script>
+
+<style>
+.home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
