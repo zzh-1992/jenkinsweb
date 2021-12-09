0f2eb8cd1fa4b73433c69af5a02771ea5b2de048
zzh-1992 <ezzh1992@icloud.com>
2021-12-09 22:42:38 +0800

    定义column-key和@filter-change="_filterChange"

--- src/views/TableFilter.vue
+++ src/views/TableFilter.vue
@@ -6,6 +6,7 @@
     row-key="date"
     :data="tableData"
     style="width: 100%"
+    @filter-change="_filterChange"
   >
     <el-table-column
       prop="date"
@@ -21,7 +22,21 @@
       ]"
       :filter-method="filterHandler"
     />
-    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column
+      prop="name"
+      label="Name"
+      :filters="[
+        { text: 'Tom1', value: 'Tom1' },
+        { text: 'Tom2', value: 'Tom2' },
+        { text: 'Tom3', value: 'Tom3' },
+        { text: 'Tom4', value: 'Tom4' },
+        { text: 'Tom5', value: 'Tom5' },
+      ]"
+      column-key="name"
+      :filter-method="filterName"
+      filter-placement="bottom-end"
+      width="180"
+    />
     <el-table-column prop="address" label="Address" :formatter="formatter" />
 
     <el-table-column
@@ -29,9 +44,13 @@
       label="Tag"
       width="100"
       :filters="[
-        { text: 'Home', value: 'Home' },
-        { text: 'Office', value: 'Office' },
+        { text: 'A', value: 'A' },
+        { text: 'B', value: 'B' },
+        { text: 'C', value: 'C' },
+        { text: 'D', value: 'D' },
+        { text: 'E', value: 'E' },
       ]"
+      column-key="tag"
       :filter-method="filterTag"
       filter-placement="bottom-end"
     >
@@ -39,8 +58,8 @@
         <el-tag
           :type="scope.row.tag === 'Home' ? '' : 'success'"
           disable-transitions
-          >{{ scope.row.tag }}</el-tag
-        >
+          >{{ scope.row.tag }}
+        </el-tag>
       </template>
     </el-table-column>
   </el-table>
@@ -50,51 +69,91 @@
 export default {
   data() {
     return {
+      // 定义数组存储用户的选项(想看到的值)
+      cName: [],
+      cTag: [],
+      // 列表数据
       tableData: [
         {
           date: "2016-05-03",
-          name: "Tom",
+          name: "Tom1",
           address: "No. 189, Grove St, Los Angeles",
-          tag: "Home",
+          tag: "A",
         },
         {
           date: "2016-05-02",
-          name: "Tom",
+          name: "Tom2",
           address: "No. 189, Grove St, Los Angeles",
-          tag: "Office",
+          tag: "A",
         },
         {
           date: "2016-05-04",
-          name: "Tom",
+          name: "Tom3",
           address: "No. 189, Grove St, Los Angeles",
-          tag: "Home",
+          tag: "B",
         },
         {
           date: "2016-05-01",
           name: "Tom",
           address: "No. 189, Grove St, Los Angeles",
-          tag: "Office",
+          tag: "C",
+        },
+        {
+          date: "2016-05-05",
+          name: "TomCat",
+          address: "No. 199, Grove St, Los Angeles",
+          tag: "D",
+        },
+        {
+          date: "2016-05-05",
+          name: "TomCatF",
+          address: "No. 199, Grove St, Los Angeles",
+          tag: "E",
         },
       ],
     };
   },
   methods: {
+    _filterChange(filter) {
+      // 该次选取的条件
+      if (filter.tag) {
+        this.cTag = filter.tag.toString().split(",");
+        console.log("cTag:" + this.cTag);
+      }
+      if (filter.name) {
+        this.cName = filter.name.toString().split(",");
+        console.log("cName:" + this.cName);
+      }
+      this.totalSize = this.$refs.filterTable.data.length;
+      console.log("_filterChange:" + this.totalSize);
+    },
+
     resetDateFilter() {
       this.$refs.filterTable.clearFilter("date");
     },
     clearFilter() {
+      // 清空用户的过滤条件
+      this.cName = [];
+      this.cTag = [];
       this.$refs.filterTable.clearFilter();
     },
     formatter(row, column) {
       console.log("column:" + column);
       return row.address;
     },
+    filterName(value, row) {
+      return row.name === value;
+    },
     filterTag(value, row) {
       return row.tag === value;
     },
     filterHandler(value, row, column) {
       const property = column["property"];
-      return row[property] === value;
+      var equal = row[property] === value;
+      console.log("value:" + value);
+      console.log("row:" + row);
+      console.log("column:" + column);
+      return equal;
     },
   },
 };
