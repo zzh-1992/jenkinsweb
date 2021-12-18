1ccbf9a8cf687ff23e77f7015d7715475ce69d21
zzh-1992 <ezzh1992@icloud.com>
2021-12-18 23:33:02 +0800

    tmp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to My Web" />
+    <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
+    <!--    <HelloWorld msg="Welcome to My Web" />-->
+    <div class="markdown">
+      <Pro />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "../components/HelloWorld.vue";
+import Pro from "../components/components/pro/index.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    //HelloWorld,
+    Pro,
   },
 };
 </script>
@@ -22,7 +27,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 </style>
