ad94da290c47401398e79771bbb80ccb847ce135
zzh-1992 <ezzh1992@icloud.com>
2021-12-19 13:57:25 +0800

    格式化处理

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,18 +1,19 @@
-
 /*
-* 保存文件到本地
-* */
+ * 保存文件到本地
+ * */
 export function saveFile(fileData, name) {
-  var pom = document.createElement('a');
-  pom.setAttribute('href', 'data:text/plain;charset=UTF-8,' + encodeURIComponent(fileData));
-  pom.setAttribute('download', name);
-  pom.style.display = 'none';
+  var pom = document.createElement("a");
+  pom.setAttribute(
+    "href",
+    "data:text/plain;charset=UTF-8," + encodeURIComponent(fileData)
+  );
+  pom.setAttribute("download", name);
+  pom.style.display = "none";
   if (document.createEvent) {
-    const event = document.createEvent('MouseEvents');
-    event.initEvent('click', true, true);
+    const event = document.createEvent("MouseEvents");
+    event.initEvent("click", true, true);
     pom.dispatchEvent(event);
   } else {
     pom.click();
   }
 }
-
