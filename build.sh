668fc4ade3a34593424ff850efe800a213345a6c
zzh-1992 <ezzh1992@icloud.com>
2021-12-19 14:00:07 +0800

    脚本测试

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "=============================================="
 ##cnpm install node-sass --save-dev
 ##cnpm install sass-loader --save-dev
 
-cnpm install
+#cnpm install
 
 cnpm run build_pro
 
