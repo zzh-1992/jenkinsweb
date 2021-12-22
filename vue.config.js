804c57b75f829b9b076f287ad502d3f7e8def60e
zzh-1992 <ezzh1992@icloud.com>
2021-12-22 20:54:24 +0800

    打包的时候添加版本号
    (cherry picked from commit c80b4651ae31477f0fe387b674fd6494ee3ed6c8)

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+// 时间戳保证不会版本重复
+const timestamp = new Date().getTime();
 module.exports = {
   productionSourceMap: false,
+  configureWebpack: { // webpack 配置
+    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+      filename: `[name].${process.env.VUE_APP_Version}.${timestamp}.js`,
+      chunkFilename: `[name].${process.env.VUE_APP_Version}.${timestamp}.js`
+    },
+  }
 };
