55cb63dc45b5c4d66617db8698ad7ea390e53ece
zzh-1992 <ezzh1992@icloud.com>
2022-01-23 10:41:57 +0800

    vue项目部署在非根目录

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // 时间戳保证不会版本重复
 const timestamp = new Date().getTime();
 module.exports = {
+  publicPath: '/grape/',
   productionSourceMap: false,
   configureWebpack: { // webpack 配置
     output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
