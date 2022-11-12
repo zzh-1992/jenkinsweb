53513c7f9b92386f213cbd31737337b4f2835e38
zzh-1992 <ezzh1992@icloud.com>
2022-11-12 12:14:48 +0800

    分离js

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,28 @@ module.exports = {
       filename: `[name].${process.env.VUE_APP_Version}.${timestamp}.js`,
       chunkFilename: `[name].${process.env.VUE_APP_Version}.${timestamp}.js`
     },
+    // 开启分离js
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace("@", "")}`;
+            },
+          },
+        },
+      },
+    },
   }
 };
